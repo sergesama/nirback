cf29837feacc4139aaec4ddca77ecb2d7bce45aa
sergesama <sergesamas@gmail.com>
2021-05-05 14:59:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var authenticate = require('./authenticate');
 // Connection URL
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
-    useMongoClient: true
+    useNewUrlParser: true
 
     /* other options */
   });
@@ -40,15 +40,6 @@ connect.then((db) => {
 
 var app = express();
 
-// Secure traffic only
-app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next();
-  }
-  else {
-    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
-  }
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
