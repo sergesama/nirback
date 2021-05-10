3bc107a864ab746ea25b6958430e50e8bd268348
Sergey Gordon <sergesamas@gmail.com>
2021-05-10 16:01:34 +0300

    assessment profiles router v1.0

---
+++ routes/assessmentprofileRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('./cors');
+var authenticate = require('../authenticate');
+const AssessmentProfiles = require('../models/assessmentprofiles');
+
+const assessmentprofileRouter = express.Router();
+
+assessmentprofileRouter.use(bodyParser.json());
+
+assessmentprofileRouter.route('/')
+.options(cors.cors, (req, res) => { res.sendStatus(200); })
+.get(cors.cors, (req,res,next) => {
+    AssessmentProfiles.find(req.query)
+      .then(
+        (assessmentprofiles) => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(assessmentprofiles);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+module.exports = assessmentprofileRouter;
