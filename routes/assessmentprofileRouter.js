66bd5d84417b4008e9fe0ec4d3775bf9f20cccd8
Sergey Gordon <sergesamas@gmail.com>
2021-05-12 17:23:10 +0300

    Add routing for filledassessmentprofiles v1.8

--- routes/assessmentprofileRouter.js
+++ routes/assessmentprofileRouter.js
@@ -13,7 +13,31 @@ assessmentprofileRouter.use(bodyParser.json());
 assessmentprofileRouter.route('/')
 .options(cors.cors, (req, res) => { res.sendStatus(200); })
 .get(cors.cors, (req,res,next) => {
-    AssessmentProfiles.find(req.query)
+    AssessmentProfiles.find({"filled":false})
+    .populate('assessment')
+    .populate('evaluator')
+    .populate('evaluated')
+    .populate({
+        path: "competence_profile", 
+        populate: {
+           path: "competences.competence_id" 
+        }
+     })
+      .then(
+        (assessmentprofiles) => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(assessmentprofiles);
+        },
+        (err) => next(err)
+      )
+      .catch((err) => next(err));
+  })
+
+  assessmentprofileRouter.route('/filled')
+.options(cors.cors, (req, res) => { res.sendStatus(200); })
+.get(cors.cors, (req,res,next) => {
+    AssessmentProfiles.find({"filled":true})
     .populate('assessment')
     .populate('evaluator')
     .populate('evaluated')
